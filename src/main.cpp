230ad7501ffef99128d04cf560eda8f3c00b55ab
nickvanhooff <nickvanhooff03@gmail.com>
2024-09-30 14:42:33 +0200

    Enhance button functionality to support multiple LED activations and blink 3 times

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,74 @@
 #include <Arduino.h>
 #include <Display.h>
 
-const int buttonPin = 8; // Pin waar de button is verbonden
+const int buttonPin = 8;  // Pin waar de button is verbonden
 const int buttonPin2 = 9; // Pin waar de button is verbonden
-const int ledPin = 4;    // Pin waar de LED is verbonden
+const int ledPin = 4;     // Pin waar de LED is verbonden
 const int ledPin2 = 6;    // Pin waar de LED is verbonden
+int count = 3;
+void setup()
+{
+  // Initialiseer de seriële poort
+  Serial.begin(9600);
 
-void setup() {
-    // Initialiseer de seriële poort
-    Serial.begin(9600);
+  // Initialiseer de LED pin als output
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 
-    // Initialiseer de LED pin als output
-    pinMode(ledPin, OUTPUT);
-    pinMode(ledPin2, OUTPUT);
+  // Initialiseer de button pin als input met pull-up weerstand
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
 
-    // Initialiseer de button pin als input met pull-up weerstand
-    pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(buttonPin2, INPUT_PULLUP);
-
-    // Initialiseer het display
-    Display.show("Ready"); // Laat een startbericht zien
+  // Initialiseer het display
+  Display.show("Ready"); // Laat een startbericht zien
 }
 
-void turnLedOn() {
+void turnLedOn()
+{
     digitalWrite(ledPin, HIGH);
     Display.show("pres");
     delay(400);
     digitalWrite(ledPin, LOW);
     Display.show("off");
-}
+    delay(400);
 
-void turnLed2On() {
-    digitalWrite(ledPin2, HIGH);
-    Display.show("pres");
- delay(400);
-    digitalWrite(ledPin2, LOW);
-    Display.show("off");
 
 }
 
-void loop() {
-    // Lees de status van de button
-    int buttonState = digitalRead(buttonPin);
-    int buttonState2 = digitalRead(buttonPin2);
+void turnLed2On()
+{
 
-    // Controleer of de button is ingedrukt
-    if (buttonState == LOW) {
-        turnLedOn();
-    } else if (buttonState2 == LOW) {
-        turnLed2On();
+  digitalWrite(ledPin2, HIGH);
+  Display.show("pres");
+  delay(400);
+  digitalWrite(ledPin2, LOW);
+  Display.show("off");
+    delay(400);
+
+}
+
+void loop()
+{
+  // Lees de status van de button
+  int buttonState = digitalRead(buttonPin);
+  int buttonState2 = digitalRead(buttonPin2);
+
+  // Controleer of de button is ingedrukt
+  if (buttonState == LOW)
+  {
+    for (int i = 0; i < count; i++)
+    {
+      turnLedOn();
     }
+  }
+  else if (buttonState2 == LOW)
+  {
+    for (int i = 0; i < count; i++)
+    {
+      turnLed2On();
+    }
+  }
 
-    // Wacht een korte tijd om stuiteren van de button te voorkomen
-    delay(50);
+  // Wacht een korte tijd om stuiteren van de button te voorkomen
+  delay(50);
 }
